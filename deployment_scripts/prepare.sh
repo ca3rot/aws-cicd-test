a307530a2e38fc01abdbbbc8f21b03862abc45a7
Jeen Lee <aiatejin@gmail.com>
2019-07-17 12:36:17 +0900

    Add `prepare.sh`

--- deployment_scripts/prepare.sh
+++ deployment_scripts/prepare.sh
@@ -3,7 +3,5 @@
 # I want to make sure that the directory is clean and has nothing left over from
 # previous deployments. The servers auto scale so the directory may or may not
 # exist.
-if [ -d /home/ec2-user/aws-cicd-test ]; then
-    rm -rf /home/ec2-user/aws-cicd-test
-fi
-mkdir -vp /home/ec2-user/aws-cicd-test
+rm -rf /home/ec2-user/aws-cicd-test/routes/*
+rm -rf /home/ec2-user/aws-cicd-test/package.json
