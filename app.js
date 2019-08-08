6f97d9685aa4b74957a7df7ee6fa34ad501a8c5b
Jeen Lee <aiatejin@gmail.com>
2019-08-08 16:09:34 +0900

    blahblah

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello MyRealTrip!!!!!');
+    res.send('Hello MUSINSA!!!!!');
 });
 
 app.listen(3000, function() {
